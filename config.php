045cea545fd8124e3b9e76cbe29b5178fde6d10c
Yous Yoeun <yousyoeun@Apples-MacBook-Pro.local>
2025-04-27 11:54:18 +0700

    f

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 	if (!$dbConnect)
 		die("<p>The database server is notavailable.</p>");
 	//echo "<p>Successfully connected to the database server.</p>";
-	$dbSelect = @mysqli_select_db($dbConnect, "database-1");
+	$dbSelect = @mysqli_select_db($dbConnect, database: "mysql");
 	if (!$dbSelect)
 		die("<p>The database is not available.</p>");
 	//echo "<p>Successfully opened the database.</p>";
